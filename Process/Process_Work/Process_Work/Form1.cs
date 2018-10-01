a7c3d507b902f2b2efc0f52a52d5cde35e3ffcf0
VladyslavSH <manujlenkovlad2@gmail.com>
2018-10-01 20:35:57 +0300

    Finished HW

---
+++ Process/Process_Work/Process_Work/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Process_Work
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                listView1.Items.Add(($"Name: {process.ProcessName} Id: {process.Id}"));
+            }
+        }
+    }
+}
